b42dd366f1255986b9992f1ab7657fa8dc00f174
rmautia <raphaelnyangenya@gmail.com>
2020-04-26 15:22:16 +0300

    add initializing application.

---
+++ app/main/views.py
@@ -0,0 +1,11 @@
+from flask import render_template,request,redirect,url_for
+from . import main
+
+# Views
+@main.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+    return render_template('index.html')
