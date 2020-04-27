1acb3929e83b8d5205974cbb103540d6295eee26
rmautia <raphaelnyangenya@gmail.com>
2020-04-27 14:30:47 +0300

    add update source-test.py //pass.

--- app/source_test.py
+++ app/source_test.py
@@ -11,7 +11,7 @@ class SourceTest(unittest.TestCase):
         '''
         Method to run before every Test
         '''
-        self.new_source= Source( "ABC News","Your trusted source for breaking news, analysis, exclusive interviews, headlines, and videos at ABCNews.com.","http://abcnews.go.com","general")
+        self.new_source= Source( "ABC News","Your trusted source for breaking news, analysis, exclusive interviews, headlines, and videos at ABCNews.com.","http://abcnews.go.com","2020-04-26T23:53:06Z","general", "Your aunt's YouTube videos about how coronavirus is a bioweapon")
 
     def test_instance(self):
         self.assertTrue(isinstance(self.new_source,Source))
